eee85dc9b1ce2d9351ae33f1a49d27ab574012db
CyberKiller <CyberKiller@users.noreply.github.com>
2019-02-09 23:53:01 +0000

    Removed name mangling

--- animals/animals.py
+++ animals/animals.py
@@ -30,14 +30,14 @@ class Animals(commands.Cog):
     async def dog2(self, ctx):
         """Get a random dog picture from www.randomdoggiegenerator.com"""
         msg = await ctx.send("`Searching for a dog...`")
-        await self.__getAndUploadDynamicJpeg(ctx, "http://www.randomdoggiegenerator.com/randomdoggie.php")
+        await self._getAndUploadDynamicJpeg(ctx, "http://www.randomdoggiegenerator.com/randomdoggie.php")
         await msg.delete()
 
     @commands.command()
     async def kitten(self, ctx):
         """Get a random kitten picture from www.randomkittengenerator.com"""
         msg = await ctx.send("`Searching for a kitten...`")
-        await self.__getAndUploadDynamicJpeg(ctx, "http://www.randomkittengenerator.com/cats/rotator.php")
+        await self._getAndUploadDynamicJpeg(ctx, "http://www.randomkittengenerator.com/cats/rotator.php")
         await msg.delete()
         
     @commands.command()
@@ -49,7 +49,7 @@ class Animals(commands.Cog):
                 result = await response.json()
                 await msg.edit(content="https://openpuppies.com/mp4/" + random.choice(result) + ".mp4")
         
-    async def __getAndUploadDynamicJpeg(self, ctx, url):
+    async def _getAndUploadDynamicJpeg(self, ctx, url):
         """Gets a jpeg from a dynamic link such as a php link then uploads it to the channel.
         This is a work-around for the discord client caching the link's dynamic content content."""
         async with aiohttp.ClientSession() as session:
