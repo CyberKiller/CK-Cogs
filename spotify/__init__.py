a2fb1d3ccc31f5a77b84ff719b617424cec10835
CyberKiller <CyberKiller@users.noreply.github.com>
2019-02-10 15:14:20 +0000

    Fix youtube search flooding.
    Added a track queue for the spotify cog that queues a track into the audio cog when a track starts playing.

--- spotify/__init__.py
+++ spotify/__init__.py
@@ -2,7 +2,7 @@ from .spotify import Spotify
 from redbot.core import commands
 
 async def setup(bot: commands.Bot):
-    cog = Spotify()
+    cog = Spotify(bot)
     await cog.initialize()
     bot.add_cog(cog)
 
