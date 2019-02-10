767813b9f58dbbee625863f8b244de2c637ca542
CyberKiller <CyberKiller@users.noreply.github.com>
2019-02-10 02:09:13 +0000

    Minor Refactoring
    Changed camel case to use underscores

--- animals/animals.py
+++ animals/animals.py
@@ -30,14 +30,14 @@ class Animals(commands.Cog):
     async def dog2(self, ctx):
         """Get a random dog picture from www.randomdoggiegenerator.com"""
         msg = await ctx.send("`Searching for a dog...`")
-        await self._getAndUploadDynamicJpeg(ctx, "http://www.randomdoggiegenerator.com/randomdoggie.php")
+        await self._get_and_upload_dynamic_jpg(ctx, "http://www.randomdoggiegenerator.com/randomdoggie.php")
         await msg.delete()
 
     @commands.command()
     async def kitten(self, ctx):
         """Get a random kitten picture from www.randomkittengenerator.com"""
         msg = await ctx.send("`Searching for a kitten...`")
-        await self._getAndUploadDynamicJpeg(ctx, "http://www.randomkittengenerator.com/cats/rotator.php")
+        await self._get_and_upload_dynamic_jpg(ctx, "http://www.randomkittengenerator.com/cats/rotator.php")
         await msg.delete()
         
     @commands.command()
@@ -49,14 +49,14 @@ class Animals(commands.Cog):
                 result = await response.json()
                 await msg.edit(content="https://openpuppies.com/mp4/" + random.choice(result) + ".mp4")
         
-    async def _getAndUploadDynamicJpeg(self, ctx, url):
+    async def _get_and_upload_dynamic_jpg(self, ctx, url):
         """Gets a jpeg from a dynamic link such as a php link then uploads it to the channel.
         This is a work-around for the discord client caching the link's dynamic content content."""
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
                 response.raise_for_status() #raise exception if status code >= 400
                 with BytesIO(await response.read()) as tmpStrm:
-                    fileExt = imghdr.what(tmpStrm) #determine the image file extention
-                    if fileExt == None: #data is not an image file!
+                    file_ext = imghdr.what(tmpStrm) #determine the image file extention
+                    if file_ext == None: #data is not an image file!
                         raise RuntimeError("Got file data that was not an image.")
-                    await ctx.send(file=discord.File(tmpStrm, "image." + fileExt))
+                    await ctx.send(file=discord.File(tmpStrm, "image." + file_ext))
